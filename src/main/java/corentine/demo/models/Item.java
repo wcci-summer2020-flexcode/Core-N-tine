903f2059fc08c07e29e20b0dc6a32b6a388ccc9b
Allen Haney <ptl4orange@yahoo.com>
2020-07-12 16:13:43 -0400

    paired programming

---
+++ src/main/java/corentine/demo/models/Item.java
@@ -0,0 +1,53 @@
+package corentine.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "items")
+    private Collection<Exercise> exercises;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Collection<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public Item() { }
+
+    public Item(String name, Exercise... exercises) {
+        this.name = name;
+        this.exercises = new ArrayList<>(Arrays.asList(exercises));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/corentine/demo/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package corentine.demo.repository;
+
+import corentine.demo.models.Exercise;
+import corentine.demo.models.Item;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Long> {
+}
